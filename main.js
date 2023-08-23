f4a54757cdd57f6b0281f5313bcaa068bea2ff8c
ADImantra <C.S.Minty@Gmail.com>
2023-08-22 17:00:07 -0700

    gameController is able to recognize a winner

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const gameBoard = (() => {
 
     const _board = new Array(9)
 
-    const getBoard = () => _board;
-
     const setBoard = () => {
 
         player = gameController.getTurn();
@@ -16,6 +14,8 @@ const gameBoard = (() => {
             return
         }
     };
+
+    const getBoard = () => _board;
 
     const printBoard = () => {
         console.log(_board)
@@ -46,6 +46,8 @@ const gameController = (
 
     let _currentTurn = _players[0];
 
+    let _winner = {};
+
     const getTurn = () => _currentTurn;
 
     const setTurn = () => {
@@ -59,19 +61,55 @@ const gameController = (
         _move = _input
     };
 
+    const getWinner = () => {
+        setWinner();
+        return _winner;
+    };
 
-    return {getTurn, setTurn, getMove, setMove}
+    const setWinner = () => {
+
+        const board = gameBoard.getBoard();
+        console.log(typeof board[0])
+
+        if (board[0] === board[1] && board[1]  === board[2]) 
+            {
+                console.log(`registered winner`)
+                Object.assign(_winner, _currentTurn)
+            }
+
+    };
+
+
+    return {getTurn, setTurn, getMove, setMove, getWinner, setWinner}
 
 })();
 
 const render = (() => {
-    
-    do {
+    const board = gameBoard.getBoard();
+    let winner = gameController.getWinner();
+    let _count = 0;
+
+    while (_count < 9 && winner.length === 0) {
         gameController.setMove();
         gameBoard.setBoard();
         gameBoard.printBoard();
-        gameController.setTurn();
-    } while (gameBoard.getBoard.length !== 9);
+
+        if (Object.keys(winner.length === 0)) {
+            gameController.setTurn();
+            console.log(`repeat`)
+        } else {
+            console.log(`${gameController.getTurn().name} is the winner`);
+            break;
+        }
+
+        ++_count
+    }
     
 
 })();
+
+/*
+    [0][1][2]
+    [3][4][5]
+    [6][7][8]
+*/
