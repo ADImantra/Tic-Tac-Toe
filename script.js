9b81b1c3cb878d14c1e4a46a851ad9bbbe1b14b3
ADImantra <C.S.Minty@Gmail.com>
2023-08-25 20:51:30 -0700

    basic board in HTML/CSS

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 import Board from "./Classes/board.js";
 import Player from "./Classes/player.js";
 
-const _displayController = (() => {
+const gameAI = () => {
+
+    let _diff = 0;
+
+    const _setDifficulty = (diff) => {
+        _diff = diff;
+    }
+
+    const _getDifficulty = () => _diff
+
+    return {
+        _setDifficulty,
+        _getDifficulty
+
+        }
+
+};
+
+const gameController = () => {
+
+    let _players = [];
+    let _mode = `AI`;
+
+    const _manageTurn = () => {
+
+    };
+
+    const _setMode = () => {
+        _mode = _mode === `1-Vs-1` ? `AI` : `1-Vs-1`;
+    };
+
+};
+
+const displayController = (() => {
     const board = new Board([``, ``, ``, ``, ``, ``, ``, ``, ``, ]);
+    const _gameBox = document.querySelectorAll(`.space`)
+    
+    _gameBox.forEach((element) => {
+        if (element ===_gameBox[0] || 
+            element === _gameBox[2] || 
+            element === _gameBox[6] ||
+            element === _gameBox[8]) {
+                element.
+            }
+    })
+
 
     board._printFormattedBoard();
 })();
