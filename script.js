281f637efb8de00011235f870e9c25127ea4adeb
ADImantra <C.S.Minty@Gmail.com>
2023-08-31 08:32:59 -0700

    Began building game logic. Players can now place tokens

--- script.js
+++ script.js
@@ -21,27 +21,44 @@ const difficultyProbability = (percentage) => {
 };
 
 const gameOn = () => {
-    
+    return board._getTerminalState();
 }
 
 const domManager = (() => {
     const _container = document.querySelector(`.container`);
     const _playSpace = document.querySelector(`.playSpace`);
-    const _spaces = document.querySelectorAll(`.block`);
+    
 
     const _resetBoard = () => {
         const _reset = document.querySelector(`.reset`);
         _reset.addEventListener(`click`, () => {
             board._clear();
         })
+    };
+
+    const _triggerStep = () => {
+        const _spaces = document.querySelectorAll(`.block`);
+        _spaces.forEach((space, index) => space.addEventListener(`click`, () => {
+            if (!board._getTerminalState(player1.token).terminal || !board._getTerminalState(player2.token).terminal) {
+                board._placeToken(player1.token, index)
+                space.textContent = player1.token
+                board._placeToken(player2.token, parseInt(player2.getBestMove(board)))
+                space.textContent = player2.token
+            }
+            
+        }))
     }
 
     return {
-        _resetBoard
+        _resetBoard,
+        _triggerStep
     }
 })();
 
+init()
 domManager._resetBoard()
+domManager._triggerStep();
+
 
 
 
